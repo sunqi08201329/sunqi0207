41f467e4ca4c74a162bd1041413bcef152b2ba15
sun qi <sunqi08201329@github.com>
2012-03-08 00:59:59 +0800

    master

---
+++ lab_Makefile/chapter1/head.h
@@ -0,0 +1,5 @@
+#ifndef HEAD_H
+#define HEAD_H
+int add(int, int);
+int mul(int, int);
+#endif
--- lab_Makefile/chapter1/hello.c
+++ lab_Makefile/chapter1/hello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "head.h"
 
 int main(int argc, const char *argv[])
 {
 	fprintf(stdout, "Hello World!\n");
-	return 0;
+	fprintf(stdout, "add:%d\n", add(1, 3));
+	fprintf(stdout, "mul:%d\n", mul(1, 3));
+	return  0;
 }
