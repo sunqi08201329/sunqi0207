ed354681774d0ced1457605dc14392d90be82d51
sun qi <sunqi08201329@github.com>
2012-03-08 12:14:25 +0800

    master

--- Node.c
+++ Node.c
@@ -1,2 +1,10 @@
-
 这样一种解决方案所带来的问它所要解决的问题更加糟糕
+explict rule:
+partern rule:
+implicit rule:
+suffix rule:
+static partern rule:
+
+make will extend wildcard when it read Makefile,but shell extexd wildcard when it excute.
+
+
