5ad2d9c598ee94596e395395bd04bac48b4764f4
sunqi123 <sq08201329@gmail.com>
2012-03-30 09:06:00 +0800

    master

---
+++ part_of_speech.c
@@ -0,0 +1,62 @@
+词性part of speech
+名词 noun  
+专有名词 proper noun  
+普通名词 common noun  
+可数名词 countable noun  
+不可数名词 uncountable noun  
+抽象名词 abstract noun  
+具体名词 concret noun  
+物质名词 material noun  
+集体名词 collective noun  
+个体名词 individual noun  
+介词 preposition  
+连词 conjunction  
+动词 verb  
+主动词 main verb  
+及物动词 transitive verb  
+不及物动词 intransitive verb  
+系动词 link verb  
+助动词 auxiliary verb  
+情态动词 modal verb  
+规则动词 regular verb  
+不规则动词 irregular verb  
+短语动词 phrasal verb  
+限定动词 finite verb  
+非限定动词 infinite verb  
+使役动词 causative verb  
+感官动词 verb of senses  
+动态动词 event verb  
+静态动词 state verb  
+感叹词 exclamation  
+形容词 adjective  
+副词 adverb  
+方式副词 adverb of manner  
+程度副词 adverb of degree  
+时间副词 adverb of time  
+地点副词 adverb of place  
+修饰性副词 adjunct  
+连接性副词 conjunct  
+疑问副词 interrogative adverb  
+关系副词 relative adverb  
+
+代词 pronoun  
+人称代词 personal pronoun  
+物主代词 possessive pronoun  
+反身代词 reflexive pronoun  
+相互代词 reciprocal pronoun  
+指示代词 demonstrative pronoun  
+疑问代词 interrogative pronoun  
+关系代词 relative pronoun  
+不定代词 indefinite pronoun  
+物主代词 possessive pronoun  
+名词性物主代词 nominal possessive pronoun  
+形容词性物主代词 adjectival possessive pronoun  
+
+冠词 article  
+定冠词 definite article  
+不定冠词 indefinite article  
+
+数词 numeral  
+基数词 cardinal numeral  
+序数词 ordinal numeral  
+分数词 fractional numeral
